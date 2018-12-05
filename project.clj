00e554c5e87c6d31a43a75f413f5a326ec365bee
vurosevic <35973264+vurosevic@users.noreply.github.com>
2018-12-05 08:41:49 +0100

    init

--- project.clj
+++ project.clj
@@ -3,4 +3,23 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :jvm-opts ["-Xms1024m" "-Xmx8000m"]
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [mount "0.1.11"]
+                 [org.datavec/datavec-api "1.0.0-beta2"]
+                 [org.deeplearning4j/deeplearning4j-nlp "1.0.0-beta2"]
+                 [org.deeplearning4j/deeplearning4j-core "1.0.0-beta2"]
+                 [org.deeplearning4j/deeplearning4j-nn "1.0.0-beta2"]
+                 [org.deeplearning4j/deeplearning4j-ui_2.11 "1.0.0-beta2"]
+                 [org.nd4j/nd4j-common "1.0.0-beta2"]
+                 [org.nd4j/nd4j-native-platform "1.0.0-beta2"]
+                 [org.slf4j/slf4j-log4j12 "1.7.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]])
---
+++ src/java/javasrc/NeuralNet.java
@@ -0,0 +1,35 @@
+package javasrc;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;package javasrc;
+
+public class NeuralNet {
+
+    public static double momentum = 0.7;
+    public static double learning_rate = 0.0015;
+
+    public static MultiLayerConfiguration getNetConfiguration () {
+        return new NeuralNetConfiguration.Builder()
+                .seed(123L)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(new Nesterovs(learning_rate, momentum))
+                .weightInit(WeightInit.XAVIER)
+                .l2(0.0001)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(64).nOut(100)
+                        .activation(Activation.TANH).build())
+                .layer(1, new DenseLayer.Builder().nIn(100).nOut(100)
+                        .activation(Activation.TANH).build())
+                .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.SQUARED_LOSS)
+                        .activation(Activation.TANH)
+                        .nIn(100).nOut(1).build())
+                .pretrain(false).backprop(true).build();
+    }
+}
